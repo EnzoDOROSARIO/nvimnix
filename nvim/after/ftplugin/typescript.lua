24c25469e73f2508f606bea26acdc6e7a29d5206
Enzo DO ROSARIO <contact@edorosario.fr>
2024-09-19 00:50:51 +0200

    testing

--- nvim/after/ftplugin/typescript.lua
+++ nvim/after/ftplugin/typescript.lua
@@ -1,10 +1,10 @@
-if vim.fn.executable('tsserver') ~= 1 then
+if vim.fn.executable('ts_ls') ~= 1 then
   return
 end
 
 vim.lsp.start({
     name = 'TypeScript Language Server',
-    cmd = { 'tsserver', '--stdio' },
+    cmd = { 'ts_ls', '--stdio' },
     capabilities = require('user.lsp').make_client_capabilities(),
     root_dir = vim.fs.dirname(vim.fs.find({'package.json', '.git'}, { upward = true })[1])
 })
