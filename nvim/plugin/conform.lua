449ce092ec87ca388bdc069a4873dcd9c3ef387d
Enzo DO ROSARIO <contact@edorosario.fr>
2024-10-02 08:23:43 +0000

    add typescriptreact for prettier

--- nvim/plugin/conform.lua
+++ nvim/plugin/conform.lua
@@ -2,6 +2,8 @@ require("conform").setup({
   formatters_by_ft = {
     javascript = { "prettier" },
     typescript = { "prettier" },
+    typescriptreact = { "prettier" },
+    javascriptreact = { "prettier" },
   },
   format_on_save = {
     timeout_ms = 500,
